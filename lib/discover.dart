e4c5196ac7929f60670136f6e4de7102dea135b9
okabasele <ocekabasele@gmail.com>
2022-04-01 17:46:57 +0200

    Update discover.dart

--- lib/discover.dart
+++ lib/discover.dart
@@ -1,10 +1,14 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
+import 'detail.dart';
+import 'functions/FirestoreHelper.dart';
 import 'model/Utilisateur.dart';
 
 class discover extends StatefulWidget {
   Utilisateur user;
   discover({required Utilisateur this.user});
+
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
@@ -17,12 +21,45 @@ class discoverState extends State<discover> {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Scaffold(
-      body: bodyPage(),
+      body: Center(
+        child: bodyPage(),
+      ),
     );
   }
-
   Widget bodyPage(){
-    return Text("explorer");
+    return StreamBuilder<QuerySnapshot>(
+        stream: FirestoreHelper().fire_user.snapshots(),
+        builder: (context, snapshot){
+          if(!snapshot.hasData){
+            return CircularProgressIndicator();
+          }
+          else
+          {
+
+            List documents = snapshot.data!.docs;
+            return ListView.builder(
+              itemCount: documents.length,
+                itemBuilder: (context,index){
+                Utilisateur user = Utilisateur(documents[index]);
+                  return ListTile(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(
+                          builder: (context){
+                            return detail(user: user);
+                          }
+                      ));
+                    },
+                    title:Text("${user.prenom}"),
+                  );
+                }
+            );
+
+          }
+        }
+    );
+
   }
+
+
 
 }
