c070b233f2b8c16695f729bad8523c981ab4d4da
okabasele <ocekabasele@gmail.com>
2022-04-05 11:35:25 +0200

    Chat visible dans chatList et discover innacessible quand on a pas d'ami

--- lib/chatList.dart
+++ lib/chatList.dart
@@ -25,54 +25,68 @@ class chatListState extends State<chatList> {
   }
 
   Widget bodyPage() {
-    return Column(
-      children: [
-        Container(
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Container(
             padding: EdgeInsets.only(top: 40.0, left: 20.0, right: 20.0),
-            child:
-                Container(
-                  alignment: AlignmentDirectional.centerStart,
-                  child: Text(
-                    "Chats",
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 30,
-                    ),
-                  ),
+            child: Container(
+              alignment: AlignmentDirectional.centerStart,
+              child: Text(
+                "Chats",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 30,
                 ),
               ),
-        const Divider(
-          height: 20,
-          thickness: 1,
-          color: Colors.grey,
-        ),
-        (widget.user.friendsUid == null)
-            ? Container(child: Text("sans ami"),)
-            : ListView.builder(
-                itemCount: widget.user.friendsUid!.length,
-                itemBuilder: (BuildContext context, int index) {
-                  Utilisateur peer = FirestoreHelper()
-                          .getUtilisateur(widget.user.friendsUid![index])
-                      as Utilisateur;
-                  return GestureDetector(
-                    onTap: () {
-                      ChatScreen(
-                        chatParams: ChatParams(widget.user, peer),
-                      );
-                    },
-                    child: Padding(
-                      padding: const EdgeInsets.only(top: 8.0),
-                      child: Card(
-                        margin: EdgeInsets.only(
-                            top: 12.0, bottom: 6.0, left: 20.0, right: 20.0),
-                        child: ListTile(
-                          title: Text(peer.prenom + " " + peer.nom!),
-                        ),
-                      ),
-                    ),
-                  );
-                }),
-      ],
+            ),
+          ),
+          const Divider(
+            height: 20,
+            thickness: 1,
+            color: Colors.grey,
+          ),
+          (widget.user.friendsUid == null)
+              ? Container(
+                  child: Text("sans ami"),
+                )
+              : Container(
+                  padding: EdgeInsets.all(20.0),
+                  height: MediaQuery.of(context).size.height - 175,
+                  width: MediaQuery.of(context).size.width,
+                  child: ListView.builder(
+                      itemCount: widget.user.friendsUid!.length,
+                      itemBuilder: (BuildContext context, int index) {
+                        return FutureBuilder(future: FirestoreHelper()
+                            .getUtilisateur(widget.user.friendsUid![index]),
+                            builder: (BuildContext context, AsyncSnapshot<Utilisateur> user) {
+                              if (!user.hasData) return Container(); // still loading
+                              // alternatively use snapshot.connectionState != ConnectionState.done
+                              final Utilisateur peer= user.data!;
+                              // return a widget here (you have to return a widget to the builder)
+                              return GestureDetector(
+                                onTap: () {
+                                  Navigator.push(context, MaterialPageRoute(builder: (context) {
+                                    return ChatScreen(
+                                      chatParams: ChatParams(widget.user, peer),
+                                    );
+                                  }));
+
+                                },
+                                child: Card(
+                                    child: ListTile(
+                                      title: Text(peer.prenom + " " + peer.nom!),
+                                    ),
+                                ),
+                              );
+                            });
+
+
+
+                      }),
+                ),
+        ],
+      ),
     );
   }
 }
